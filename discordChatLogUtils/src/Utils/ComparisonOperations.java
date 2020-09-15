37c509ed9b5f33e45d39d3bfb343acfadf22e9fb
ubcbent <bentisserand98@gmail.com>
2020-09-14 21:07:19 -0700

    Implementing a comparison operator to compare by map value

---
+++ discordChatLogUtils/src/Utils/ComparisonOperations.java
@@ -0,0 +1,27 @@
+package Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComparisonOperations {
+
+    /**
+     *
+     * @param oldWords - the first set of words, these will be subtracted from the second set
+     * @param newWords - the second set of words, these define the later stage which will have the first subtracted to find the difference
+     * @return A map containing each word and the difference the second wordset had from the first
+     */
+    public static Map<String,Integer> wordCountDifference(Map<String,Integer> oldWords, Map<String,Integer> newWords){
+        Map<String,Integer> output = new HashMap<>(newWords);
+
+        for(Map.Entry<String,Integer> entry : oldWords.entrySet()){
+            if(output.get(entry.getKey())==null){
+                output.put(entry.getKey(),entry.getValue()*-1);
+            }else{
+                output.put(entry.getKey(),(output.get(entry.getKey()).intValue())-(entry.getValue().intValue()));
+            }
+        }
+
+        return output;
+    }
+}
